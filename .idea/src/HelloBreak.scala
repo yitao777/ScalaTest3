a1b2502b9468b1b0fa80928aef9ff002ba5349ad
xiongyitao <a6700195@163.com>
2019-09-18 19:19:20 +0800

    测试提交

---
+++ .idea/src/HelloBreak.scala
@@ -0,0 +1,19 @@
+import scala.util.control.Breaks
+
+object HelloBreak {
+  def main(args: Array[String]): Unit = {
+    var break = new Breaks;
+    var a = 10;
+    break.breakable({
+      do{
+        print("\t" + a);
+        a -= 1
+        if (a<=5) {
+          break.break();
+        }
+      }while(a>0)
+    })
+  }
+
+
+}
